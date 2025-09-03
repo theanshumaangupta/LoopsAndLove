11cf6b2d470e6197811dd68e23c206eb7c6498a7
Anshumaan Gupta <theanshumangupta@gmail.com>
2025-09-04 02:25:51 +0530

    addedExplore

--- src/app/components/Explore.jsx
+++ src/app/components/Explore.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React from "react";
 
-function Explore (){
-    return (
-        <div className='flex min-h-screen border-2 border-blue-500'>
-            Explore Page
+function Explore() {
+  return (
+    <div className="bg-[#F3F0EC] text-black">
+      <div className="px-20 py-30  text-black">
+        <div className="flex justify-between text-3xl min-h-[8rem] ">
+          <div>
+            <p
+              style={{ fontFamily: "Merriweather,serif" }}
+              className="mb-5 text-3xl tracking-widest"
+            >
+              Explore New Arrivals
+            </p>
+            <p className="text-base">Shop Now</p>
+          </div>
+          <div className="flex gap-5">
+            <img className="w-12" src="/images/line-arrow.svg" alt="" />
+            <img className="w-12" src="/images/line-arrow2.svg" alt="" />
+          </div>
         </div>
-    );
+        <div className="flex gap-10">
+          <div
+            style={{ backgroundImage: 'url("/images/shop-img-1.jpg")' }}
+            className="w-[25vw] h-[50vh] bg-cover"
+          ></div>
+          <div
+            style={{ backgroundImage: 'url("/images/shop-img-2.jpg")' }}
+            className="w-[25vw] h-[50vh] bg-cover"
+          ></div>
+          <div
+            style={{ backgroundImage: 'url("/images/shop-img-3.jpg")' }}
+            className="w-[25vw] h-[50vh] bg-cover"
+          ></div>
+          <div
+            style={{ backgroundImage: 'url("/images/shop-img-4.jpg")' }}
+            className="w-[25vw] h-[50vh] bg-cover"
+          ></div>
+        </div>
+      </div>
+      <div className=" flex flex-col gap-10 items-center justify-center">
+        <h1 style={{fontFamily: 'Merriweather', fontWeight: '300'}} className=" text-3xl md:text-4xl font-serif font-bold" >Best of Handmade Goods</h1>
+        <img style={{filter: 'invert(1)'}} src="images/thread.png" alt="" />
+        <p style={{fontFamily: 'Roboto'}} className="text-base max-w-3xl text-[#4A4848] mx-auto text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod t mcididunt ut labore et dolore. Ut enim ad minim veniam, quis nostrud exercitation ullam ris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reerit in voluptate velit</p>
+        <div className="flex gap-20 mb-[12vh]" >
+            <div>
+                <img className="w-[6rem]" src="/images/h1-icon-1-x2.png" alt="" />
+                <h2>Biodegraduate</h2>
+                <p>Recent in voluptate suis amet</p>
+            </div>
+            <div>
+                <img className="w-[6rem]" src="/images/h1-icon-2-x2.png" alt="" />
+                <h2>Biodegraduate</h2>
+                <p>Recent in voluptate suis amet</p>
+            </div>
+            <div>
+                <img className="w-[6rem]" src="/images/h1-icon-3-x2.png" alt="" />
+                <h2>Biodegraduate</h2>
+                <p>Recent in voluptate suis amet</p>
+            </div>
+            <div>
+                <img className="w-[6rem]" src="/images/h1-icon-4-x2.png" alt="" />
+                <h2>Biodegraduate</h2>
+                <p>Recent in voluptate suis amet</p>
+            </div>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default Explore;
--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -1,10 +1,25 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
+import { Roboto } from "next/font/google";
+import { Merriweather } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"], 
+});
+
+
+const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["400", "700"], // choose weights you need
+});
+
+
 export default function Slider() {
   const images = [
-    "/images/slider2.jpg",
-    "/images/slider3.jpg",
-    "/images/slider5.webp",
+    "/images/rs1.jpg",
+    "/images/rs2.jpg",
+    "/images/rs3.jpg",
   ];
   const [CurrentIndex, setCurrentIndex] = useState(0);
   const [visible, setVisible] = useState(false);
@@ -17,7 +32,7 @@ export default function Slider() {
   }
 
   useEffect(() => {
-    const interval = setInterval(setNextSlide, 4000);
+    const interval = setInterval(setNextSlide, 2000);
     return () => {
       clearInterval(interval);
     };
@@ -26,7 +41,7 @@ export default function Slider() {
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => setVisible(entries[0].isIntersecting),
-      { threshold: 0.1 }
+      { threshold: 1 }
     );
     if (sliderRef.current) {
       observer.observe(sliderRef.current);
@@ -40,13 +55,25 @@ export default function Slider() {
   return (
     <div
       ref={sliderRef}
-      style={{ backgroundImage: `url(${images[CurrentIndex]})` }}
+      // backgroundSize: `${visible ? '100vw 100vh' : '100vw 105vh'}`
+      style={{ backgroundImage: `url(${images[CurrentIndex]})`, backgroundPosition: `${visible ? '0 -2vh' : '0 0'}` }}
       
       className={`h-screen relative w-full ${
-        visible ? "opacity-100" : "opacity-50"
-      } bg-cover bg-position-center transition-all duration-1000 ease-in-out` }
+        visible ? "opacity-100" : "opacity-50 "
+      } bg-cover transition-all duration-2000 ease-in-out` }
     >
-      
+      <div className="pt-10 pl-14 top-0 left-0 flex flex-col gap-3 uppercase" style={{fontFamily : 'Roboto'}} >
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>  
+        <p>Pages</p>
+        <p>Shop</p>
+      </div>
+      <div className=" flex flex-col items-center  gap-5 justify-center text-center absolute bottom-1/4 m-auto w-full text-5xl " >
+        <h1 style={{fontFamily: "Merriweather"}} className="inline-block w-[40rem] leading-[67px] text-[50px] ">Natural Wool For Comfy Winter Clothes</h1>
+        <p className="text-[1rem]" style={{fontFamily: 'Roboto'}}>Lorem ipsum dolor sit, consectuer adipliscing elit.</p>
+        <img src="/images/thread.png" alt="" />
+      </div>
     </div>
   );
 }
