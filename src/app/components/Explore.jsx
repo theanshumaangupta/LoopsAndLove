ab0f83ce260c9330f85df0e0471577efcef8ecf7
unknown <theanshumangupta@gmail.com>
2025-09-09 00:14:30 +0530

    8sep

--- src/app/components/Explore.jsx
+++ src/app/components/Explore.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./explore.css";
 function Explore() {
   return (
     <div className="bg-[#F3F0EC] text-black">
@@ -18,56 +18,69 @@ function Explore() {
           <div className="flex gap-5">
             <img className="w-12" src="/images/line-arrow.svg" alt="" />
             <img className="w-12" src="/images/line-arrow2.svg" alt="" />
-            
+
           </div>
         </div>
         {/* Images slides  */}
-        <div className="flex gap-10">
+        <div className="overflow-hidden mask-gradient">
           <div
-            style={{ backgroundImage: 'url("/images/shop-img-7.jpg")' }}
-            className="w-[25vw] h-[50vh] bg-cover"
-          ></div>
-          <div
-            style={{ backgroundImage: 'url("/images/shop-img-8.jpg")' }}
-            className="w-[25vw] h-[50vh] bg-cover"
-          ></div>
-          <div
-            style={{ backgroundImage: 'url("/images/shop-img-9.jpg")' }}
-            className="w-[25vw] h-[50vh] bg-cover"
-          ></div>
-          <div
-            style={{ backgroundImage: 'url("/images/shop-img-6.jpg")' }}
-            className="w-[25vw] h-[50vh] bg-cover"
-          ></div>
+            className="flex gap-10 overflow-hidden w-fit whitespace-nowrap animate-slide"
+          >
+            <div
+              style={{ backgroundImage: 'url("/images/shop-img-7.jpg")' }}
+              className="w-[25vw] h-[50vh] bg-cover inline-block"
+            ></div>
+            <div
+              style={{ backgroundImage: 'url("/images/shop-img-8.jpg")' }}
+              className="w-[25vw] h-[50vh] bg-cover inline-block"
+            ></div>
+            <div
+              style={{ backgroundImage: 'url("/images/shop-img-9.jpg")' }}
+              className="w-[25vw] h-[50vh] bg-cover inline-block"
+            ></div>
+            <div
+              style={{ backgroundImage: 'url("/images/shop-img-6.jpg")' }}
+              className="w-[25vw] h-[50vh] bg-cover inline-block"
+            ></div>
+            <div
+              style={{ backgroundImage: 'url("/images/shop-img-9.jpg")' }}
+              className="w-[25vw] h-[50vh] bg-cover inline-block"
+            ></div>
+            <div
+              style={{ backgroundImage: 'url("/images/shop-img-6.jpg")' }}
+              className="w-[25vw] h-[50vh] bg-cover inline-block"
+            ></div>
+          </div>
         </div>
+
       </div>
 
       {/* SKetchy CAtegories like biodegraduate */}
       <div className=" flex flex-col gap-10 items-center justify-center">
-        <h1 style={{fontFamily: 'Merriweather', fontWeight: '300'}} className=" text-3xl md:text-4xl font-serif font-bold" >Best of Handmade Goods</h1>
-        <img style={{filter: 'invert(1)'}} src="images/thread.png" alt="" />
-        <p style={{fontFamily: 'Roboto'}} className="text-base max-w-3xl text-[#4A4848] mx-auto text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod t mcididunt ut labore et dolore. Ut enim ad minim veniam, quis nostrud exercitation ullam ris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reerit in voluptate velit</p>
+        <h1 style={{ fontFamily: 'Merriweather', fontWeight: '300' }} className=" text-3xl md:text-4xl font-serif font-bold" >Best of Handmade Goods</h1>
+        <img style={{ filter: 'invert(1)' }} src="images/thread.png" alt="" />
+        <p style={{ fontFamily: 'Roboto' }} className="text-base max-w-3xl text-[#4A4848] mx-auto text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod t mcididunt ut labore et dolore. Ut enim ad minim veniam, quis nostrud exercitation ullam ris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reerit in voluptate velit</p>
         <div className="flex gap-20 mb-[12vh]" >
-            <div>
-                <img className="w-[6rem]" src="/images/h1-icon-1-x2.png" alt="" />
-                <h2>Biodegraduate</h2>
-                <p>Recent in voluptate suis amet</p>
-            </div>
-            <div>
-                <img className="w-[6rem]" src="/images/h1-icon-2-x2.png" alt="" />
-                <h2>Biodegraduate</h2>
-                <p>Recent in voluptate suis amet</p>
-            </div>
-            <div>
-                <img className="w-[6rem]" src="/images/h1-icon-3-x2.png" alt="" />
-                <h2>Biodegraduate</h2>
-                <p>Recent in voluptate suis amet</p>
-            </div>
-            <div>
-                <img className="w-[6rem]" src="/images/h1-icon-4-x2.png" alt="" />
-                <h2>Biodegraduate</h2>
-                <p>Recent in voluptate suis amet</p>
-            </div>
+          <div>
+            <img className="w-[6rem]" src="/images/h1-icon-1-x2.png" alt="" />
+            <h2>Biodegraduate</h2>
+            <p>Recent in voluptate suis amet</p>
+          </div>
+          <div>
+            <img className="w-[6rem]" src="/images/h1-icon-2-x2.png" alt="" />
+            <h2>Biodegraduate</h2>
+            <p>Recent in voluptate suis amet</p>
+          </div>
+          <div>
+            <img className="w-[6rem]" src="/images/h1-icon-3-x2.png" alt="" />
+            <h2>Biodegraduate</h2>
+            <p>Recent in voluptate suis amet</p>
+          </div>
+          <div>
+            <img className="w-[6rem]" src="/images/h1-icon-4-x2.png" alt="" />
+            <h2>Biodegraduate</h2>
+            <p>Recent in voluptate suis amet</p>
+          </div>
         </div>
       </div>
 
