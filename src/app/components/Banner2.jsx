b526c913d54bdecb20c9f8e1b80c411453b9e7ac
unknown <theanshumangupta@gmail.com>
2025-09-09 13:39:01 +0530

    fix3

--- src/app/components/Banner2.jsx
+++ src/app/components/Banner2.jsx
@@ -5,19 +5,21 @@ function Banner2() {
       style={{ backgroundImage: 'url("/images/banner2.jpg")' }}
     >
       <img
-        className="w-[20rem] absolute top-[8rem] left-[8rem]"
+        className="hidden md:block w-[20rem] absolute top-[3rem] left-[3rem]"
         src="/images/banner2-1.jpg"
         alt=""
       />
       <img
-        className="w-[20rem] absolute bottom-[8rem] right-[8rem]"
+        className="hidden md:block w-[20rem] absolute bottom-[3rem] right-[3rem]"
         src="/images/banner2-2.jpg"
         alt=""
       />
-      <div className="absolute flex flex-col gap-4 left-[5rem] bottom-[5rem]">
+      <div className="absolute flex flex-col gap-4 left-4 md:left-10 bottom-10 ">
         <img className="w-[20rem]" src="images/off30.png" alt="" />
-        <h1 style={{fontFamily: "Merriweather"}} className="text-3xl ">Explore New Arrivals</h1>
-        <p style={{fontFamily: "Roboto"}} className="text-base">Lorem ipsum dolor sit amet consectetur, contenuer</p>
+        <div className="p-2">
+          <h1 style={{fontFamily: "Merriweather"}} className="text-3xl ">Explore New Arrivals</h1>
+          <p style={{fontFamily: "Roboto"}} className="text-base">Lorem ipsum dolor sit amet consectetur, contenuer</p>
+        </div>
       </div>
     </div>
   );
