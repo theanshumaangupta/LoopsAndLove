f65f063e872c0e9d30f21c3f9bb7079cdc352ae4
unknown <theanshumangupta@gmail.com>
2025-09-10 16:46:23 +0530

    commentfix

--- src/app/components/Explore2.jsx
+++ src/app/components/Explore2.jsx
@@ -1,21 +1,23 @@
+
+import { url } from "inspector";
+
 function Explore2() {
   return (
     <>
-      {/* 5 box grid section */}
+      {/* 5 box grid section*/}
       <div className="w-full px-5 md:px-20 py-10 md:py-20 bg-[#F3F0EC] text-black flex justify-center items-center">
-        <div className="flex flex-col gap-10 max-w-[1200px] w-full">
+        <div className="flex flex-col gap-10">
           <h1
-            style={{ fontFamily: "Merriweather" }}
-            className="text-3xl md:text-5xl text-center"
+            style={{ fontFamily: "Merriweather"}}
+            className="text-5xl text-center"
           >
             Explore New Arrivals
           </h1>
           <p className="text-base text-center">
             Lorem ipsum dolor sit amet, adiping elit
           </p>
-
-          <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 auto-rows-[200px]">
-            <div className="relative sm:col-span-2 sm:row-span-2 overflow-hidden">
+          <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-4  w-[90vw] sm:w-[75vw] lg:w-[80vw] sm:auto-rows-[125px] lg:auto-rows-[300px] auto-rows-[200px]">
+            <div className="relative col-span-2 row-span-2 overflow-hidden">
               <div
                 style={{
                   backgroundImage: "url('images/shop-category-img-1.jpg')",
@@ -63,15 +65,19 @@ function Explore2() {
         </div>
       </div>
 
-      {/* Section Of Comments */}
-      <div className="w-full flex justify-center items-center py-10 bg-white">
-        <div className="flex flex-col md:flex-row justify-between items-center gap-10 md:gap-[20rem] max-w-[1200px] w-full px-5 md:px-0">
 
-          {/* Comment and Quote Sticker */}
-          <div className="flex flex-col md:flex-row items-center md:items-start gap-10">
-            <div className="relative">
+
+
+      {/* section Of Comments */}
+      <div className="w-full flex justify-center items-center min-h-[50vh] bg-white">
+
+        <div className=" flex flex-col md:flex-row p-[2rem] text-center justify-between items-start gap-[5rem] md:gap-[5rem] bg-white text-black">
+          {/* COmmement and Quote sticker holder */}
+          <div className="flex flex-col lg:flex-row items-start gap-[2rem]">
+            {/*Quote Sticker holder */}
+            <div className="flex justify-center items-center relative">
               <svg
-                className="absolute left-0 bottom-10 z-10"
+                className="left-0 bottom-10 absolute z-10 "
                 xmlns="http://www.w3.org/2000/svg"
                 width="100.798"
                 height="100.992"
@@ -83,41 +89,45 @@ function Explore2() {
                   transform="translate(8.276 0.596) rotate(10)"
                   fill="none"
                   stroke="currentColor"
-                  strokeWidth="1"
-                />
+                  stroke-width="1"
+                ></path>
                 <path
                   d="M12.132,12.918S-2.541,13.251.386,5.147s15.285-5.6,17.575.366-5.036,38.559-5.276,38.751S12.132,12.918,12.132,12.918Z"
                   transform="translate(34.086 0.596) rotate(10)"
                   fill="none"
                   stroke="currentColor"
-                  strokeWidth="1"
-                />
+                  stroke-width="1"
+                  class=""
+                ></path>
               </svg>
-              <div className="w-[200px] h-[100px] rounded-full bg-[#f3f0ec] z-[1] -rotate-[16deg]"></div>
+              <div className=" w-[250px] h-[124px] block -rotate-[16deg] rounded-[50%] bg-[#f3f0ec] z-[1]"></div>
             </div>
-
-            {/* Comment text */}
-            <div className="max-w-[600px]">
+            {/* Comment holder */}
+            <div className=" w-full ">
               <h1
                 style={{ fontFamily: "Merriweather" }}
-                className="text-xl md:text-2xl italic mb-5"
+                className="text-base md:text-2xl italic mb-5 max-w-[600px] md:max-w-[40rem] "
               >
-                Morbi enim nunc faucibus a pellentesque. Condimentum lacinia quis vel eros donec ac odio tempor orci. Massa placerat duis ultricies lacus. Adipiscing enim eu turpis.
+                Morbi enim nunc faucibus a pellentesque. Condimentum lacinia quis
+                vel eros donec ac odio tempor orci. Massa placerat duis ultricies
+                lacus. Adipiscing enim eu turpis.
               </h1>
               <div className="flex items-center gap-5">
-                <img src="images/p2-user-img.png" height="40" alt="user" />
-                <p><b>Nico Roots,</b> customer</p>
+                <img src="images/p2-user-img.png"  height="20px" alt="" />
+                <p>
+                  <b>Nico Roots,</b> customer
+                </p>
               </div>
             </div>
           </div>
 
           {/* Arrow image */}
-          <div className="flex flex-col items-center">
-            <img className="mb-5 w-10 h-10 md:w-auto md:h-auto" src="images/line-arrow.svg" alt="arrow" />
-            <img src="images/line-arrow2.svg" alt="arrow2" />
+          <div className="self-end">
+            <img className="mb-5" src="images/line-arrow.svg" alt="" />
+            <img src="images/line-arrow2.svg" alt="" />
           </div>
-
         </div>
+
       </div>
     </>
   );
