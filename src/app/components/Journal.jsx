fe6e6cc5e2a6449a655309a23d7aef6adad0b70f
unknown <theanshumangupta@gmail.com>
2025-09-11 01:44:05 +0530

    journalFix

--- src/app/components/Journal.jsx
+++ src/app/components/Journal.jsx
@@ -1,79 +1,82 @@
-
 function Journal() {
   return (
-    <div className="w-full p-10 items-stretch min-h-screen bg-[#F3F0EC] text-black ">
-      <h1 className="text-3xl mb-10 ">Journal Our Story</h1>
-      <div className=" flex justify-center items-stretch min-h-[60vh] gap-5">
+    <div className="w-full p-10 bg-[#F3F0EC] text-black">
+      <h1 className="text-3xl mb-10">Journal Our Story</h1>
 
-        <div className=" flex flex-col flex-2 text-black">
-          {/* image div with date tag */}
-          <div
-            style={{ backgroundImage: "url('images/post-img-1.jpg')" }}
-            className="min-h-[60vh] relative bg-cover bg-center"
-          >
-            {/* Date Tag */}
-            <div className=" w-[5rem] h-[2.5rem] justify-center items-center absolute top-5 left-5 -rotate-[16deg] rounded-[50%] bg-[#f3f0ec] z-[1]">
-              <p className="text-sm text-center rotate-[16deg] mt-1 text-black">
-                19 May
-              </p>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+
+        {/* Journal Item 1 */}
+        <div className="flex flex-col">
+          <div className="relative h-64">
+            <img
+              src="images/post-img-1.jpg"
+              alt="Journal Post 1"
+              className="w-full h-full object-cover"
+            />
+            <div className="w-20 h-10 flex justify-center items-center absolute top-5 left-5 -rotate-6 rounded-full bg-[#f3f0ec] z-10">
+              <p className="text-sm text-center rotate-6 mt-1 text-black">19 May</p>
             </div>
           </div>
-          {/* twxt div */}
-          <div className="flex flex-col gap-4">
-            <p className="text-base mt-5">Handmade</p>
-            <p style={{fontFamily: 'Merriweather'}} className="font-light text-3xl">Explore our custom made techniques</p>
-            <p className="text-base max-w-[90%] text-[#4A4848]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magnali qua. Ut enim
-              ad minim veniam, quis nostrud exercitation ullamc
+
+          <div className="flex flex-col gap-4 mt-5">
+            <p className="text-base">Handmade</p>
+            <p style={{ fontFamily: 'Merriweather' }} className="font-light text-3xl">
+              Explore our custom made techniques
             </p>
-            <p>Read More</p>
+            <p className="text-base text-[#4A4848]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnali qua. Ut enim ad minim veniam, quis nostrud exercitation ullamc
+            </p>
+            
           </div>
         </div>
 
-        <div className="w-[1px] bg-black" ></div>
-
-        <div className=" flex flex-col flex-1">
-          <div
-            style={{ backgroundImage: "url('images/post-list-1-img-1.jpg')" }}
-            className=" relative bg-cover bg-center min-h-[60vh] flex-1"
-          >
-            <div className=" w-[5rem] h-[2.5rem] justify-center items-center absolute top-5 left-5 -rotate-[16deg] rounded-[50%] bg-[#f3f0ec] z-[1]">
-              <p className="text-sm text-center rotate-[16deg] mt-1 text-black">
-                19 May
-              </p>
+        {/* Journal Item 2 */}
+        <div className="flex flex-col">
+          <div className="relative h-64">
+            <img
+              src="images/post-list-1-img-1.jpg"
+              alt="Journal Post 2"
+              className="w-full h-full object-cover"
+            />
+            <div className="w-20 h-10 flex justify-center items-center absolute top-5 left-5 -rotate-6 rounded-full bg-[#f3f0ec] z-10">
+              <p className="text-sm text-center rotate-6 mt-1 text-black">19 May</p>
             </div>
           </div>
-          <div className="flex flex-col gap-4">
-            <p className="text-base mt-5">Handmade</p>
-            <p style={{fontFamily: 'Merriweather'}} className="font-light text-3xl">Explore our custom made techniques</p>
-            <p className="text-base max-w-[90%] text-[#4A4848]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut.
+
+          <div className="flex flex-col gap-4 mt-5">
+            <p className="text-base">Handmade</p>
+            <p style={{ fontFamily: 'Merriweather' }} className="font-light text-3xl">
+              Explore our custom made techniques
             </p>
-            <p>Read More</p>
+            <p className="text-base text-[#4A4848]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
+            </p>
+            
           </div>
         </div>
 
-        <div className=" flex flex-col flex-1">
-          <div
-            style={{ backgroundImage: "url('images/post-list-1-img-3.jpg')" }}
-            className="relative bg-cover bg-center min-h-[60vh] flex-1"
-          >
-            <div className=" w-[5rem] h-[2.5rem] justify-center items-center absolute top-5 left-5 -rotate-[16deg] rounded-[50%] bg-[#f3f0ec] z-[1]">
-              <p className="text-sm text-center rotate-[16deg] mt-1 text-black">
-                19 May
-              </p>
+        {/* Journal Item 3 */}
+        <div className="flex flex-col">
+          <div className="relative h-64">
+            <img
+              src="images/post-list-1-img-3.jpg"
+              alt="Journal Post 3"
+              className="w-full h-full object-cover"
+            />
+            <div className="w-20 h-10 flex justify-center items-center absolute top-5 left-5 -rotate-6 rounded-full bg-[#f3f0ec] z-10">
+              <p className="text-sm text-center rotate-6 mt-1 text-black">19 May</p>
             </div>
           </div>
-          <div className="flex flex-col gap-4">
-            <p className="text-base mt-5">Handmade</p>
-            <p style={{fontFamily: 'Merriweather'}} className="font-light text-3xl">Explore our custom made techniques</p>
-            <p className="text-base max-w-[90%] text-[#4A4848]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut.
+
+          <div className="flex flex-col gap-4 mt-5">
+            <p className="text-base">Handmade</p>
+            <p style={{ fontFamily: 'Merriweather' }} className="font-light text-3xl">
+              Explore our custom made techniques
             </p>
-            <p>Read More</p>
+            <p className="text-base text-[#4A4848]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
+            </p>
+            
           </div>
         </div>
 
