5bf73f660c304ab89f8ba89972bf0b16abd01bce
unknown <theanshumangupta@gmail.com>
2025-09-14 02:03:45 +0530

    12sep

--- src/app/components/Explore.jsx
+++ src/app/components/Explore.jsx
@@ -113,13 +113,46 @@ function Explore() {
           </div>
         </div>
 
-        {/* Progress Bar */}
-        {/* <div className="relative mt-5 bottom-2 left-0 w-full h-2 bg-[#F3F0EC] rounded-full">
-          <div
-            className="h-2 bg-[#ffcfcf] rounded-full transition-all duration-300"
-            style={{ width: `${progressPercent}%` }}
-          ></div>
-        </div> */}
+        <div className="flex flex-col gap-10  mt-25 lg:p-15 text-center items-center justify-center">
+          <h1 style={{ fontFamily: "Merriweather", fontWeight: "300" }} className="text-3xl md:text-4xl font-serif font-bold">
+            Best of Handmade Goods
+          </h1>
+          <img style={{ filter: 'invert(1)' }} src="images/thread.png" alt="" />
+          <p style={{ fontFamily: 'Roboto' }} className="text-base text-gray-600 max-w-3xl mx-auto text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod t mcididunt ut labore et dolore. Ut enim ad minim veniam, quis nostrud exercitation ullam ris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reerit in voluptate velit
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-15 mb-24">
+            {/* Category items */}
+            <div className="flex flex-col justify-between items-center gap-2">
+              <img className="w-24 md:w-32" src={`/images/h1-icon-4-x2.png`} alt="" />
+              <div>
+                <h2>Biodegraduate</h2>
+                <p>Recent in voluptate suis amet</p>
+              </div>
+            </div>
+            <div className="flex flex-col justify-between items-center gap-2">
+              <img className="w-24 md:w-32" src={`/images/h1-icon-1-x2.png`} alt="" />
+              <div>
+                <h2>Biodegraduate</h2>
+                <p>Recent in voluptate suis amet</p>
+              </div>
+            </div>
+            <div className="flex flex-col justify-between items-center gap-2">
+              <img className="w-24 md:w-32" src={`/images/h1-icon-1-x2.png`} alt="" />
+              <div>
+                <h2>Biodegraduate</h2>
+                <p>Recent in voluptate suis amet</p>
+              </div>
+            </div>
+            <div className="flex flex-col justify-between items-center gap-2">
+              <img className="w-24 md:w-32" src={`/images/h1-icon-2-x2.png`} alt="" />
+              <div>
+                <h2>Biodegraduate</h2>
+                <p>Recent in voluptate suis amet</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
